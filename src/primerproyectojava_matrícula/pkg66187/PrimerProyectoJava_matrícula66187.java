288153fac538b5cddfb2dbd2a569d184bc896e1e
DELL <DELL@DELL-PC>
2021-03-01 07:49:44 -0600

    comit1

---
+++ src/primerproyectojava_matrícula/pkg66187/PrimerProyectoJava_matrícula66187.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectojava_matrícula.pkg66187;
+
+/**
+ *
+ * @author Alondra Palma
+ */
+public class PrimerProyectoJava_matrícula66187 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad = 19;
+        System.out.println("Edad = " + edad);
+        char letra = 'A';
+        System.out.println("La letra es = " + letra);
+        boolean verdadero = true;
+        System.out.println("Es verdadero o falso = " + verdadero);
+    }
+    
+}
